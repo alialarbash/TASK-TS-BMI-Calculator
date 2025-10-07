1635e6f0cbcf276851bdb90dd6030c8a38445447
alialarbash <ali.alarbash456@gmail.com>
2025-10-07 17:40:06 +0300

    Fixed the errors

--- src/BMI.ts
+++ src/BMI.ts
@@ -7,7 +7,7 @@
  */
 function calculateBMI(weight: number, height: number): number {
   // Write your code here 👇🏼
-  const BMI = 0;
+  const BMI = (10000 * weight) / height ** 2;
   // Stop making changes here!
   return BMI;
 }
@@ -25,6 +25,10 @@ function getBMIState(BMI: number): BMIStatus {
   // Don't change this variable
   let BMIState: BMIStatus = "obese";
   // Write your code here 👇🏼
+  if (BMI < 18.5) BMIState = "underweight";
+  else if (BMI >= 18.5 && BMI < 25) BMIState = "healthy";
+  else if (BMI >= 25 && BMI < 30) BMIState = "overweight";
+  else BMIState = "obese";
 
   // Stop making changes here!
   return BMIState;
@@ -44,7 +48,17 @@ function getBMIStateByAge(BMI: number, age: number): HealthStatus {
   // Don't change this variable
   let BMIState: HealthStatus = "not healthy";
   // Write your code here 👇🏼
-
+  if (19 <= age && age <= 24 && 19 <= BMI && BMI <= 24) BMIState = "healthy";
+  else if (25 <= age && age <= 34 && 20 <= BMI && BMI <= 25)
+    BMIState = "healthy";
+  else if (35 <= age && age <= 44 && 21 <= BMI && BMI <= 26)
+    BMIState = "healthy";
+  else if (45 <= age && age <= 54 && 22 <= BMI && BMI <= 27)
+    BMIState = "healthy";
+  else if (55 <= age && age <= 64 && 23 <= BMI && BMI <= 28)
+    BMIState = "healthy";
+  else if (age >= 65 && 24 <= BMI && BMI <= 29) BMIState = "healthy";
+  else BMIState = "not healthy";
   // Stop making changes here!
   return BMIState;
 }
